ae992a98e75fe6c01ea12606444298f6e1484f34
Leonardo Ferreira Fontenelle <leonardof@leonadof.med.br>
2023-06-13 15:48:49 -0300

    Load packages

---
+++ rcv_2013.R
@@ -0,0 +1,11 @@
+# Agreement between Globorisk LAC and Pooled Cohort Equations for the 
+# estimation of cardiovascular risk in Brazil, 2013
+#
+
+# Load packages ----
+
+# https://github.com/boyercb/globorisk
+load("sysdata.rda") # coefs, cvdr, rf
+source("globorisk.R")
+
+library(PooledCohort)
