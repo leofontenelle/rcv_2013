e0e314bf14501e7abac0f8295ec613a267e6e602
Leonardo Ferreira Fontenelle <leonardof@leonadof.med.br>
2023-06-14 15:09:39 -0300

    Estimate risk of cardiovascular disease

--- rcv_2013.R
+++ rcv_2013.R
@@ -12,6 +12,7 @@ library(PooledCohort)
 
 source("functions.R")
 
+
 # Read data ----
 
 # https://www.pns.icict.fiocruz.br/bases-de-dados/
@@ -21,7 +22,7 @@ pns2013_lab <- read.csv2("pns2013_exames_2023-05-05.csv",
 d <- with(pns2013_lab, data.frame(
   sex = Z001 - 1,
   age_years = Z002,
-  race = ifelse(Z003 <= 5, Z003, NA),
+  race = ifelse(Z003 <= 5, Z003, NA), # 9 is "ignored"
   smoke_current = as.integer(P050 %in% 1:2),
   chol_total_mgdl = Z031,
   chol_hdl_mgdl = Z032,
@@ -32,3 +33,41 @@ d <- with(pns2013_lab, data.frame(
 ))
 
 rm(pns2013_lab)
+
+
+# Estimate CDV risk ----
+
+d <- within(d, {
+  pooled_cohort <- predict_10yr_ascvd_risk(
+    age_years = age_years,
+    race = race,
+    sex = sex,
+    smoke_current = smoke_current,
+    chol_total_mgdl = chol_total_mgdl,
+    chol_hdl_mgdl = chol_hdl_mgdl,
+    bp_sys_mmhg = bp_sys_mmhg,
+    bp_meds = bp_meds,
+    diabetes = diabetes,
+    equation_version = "Yadlowsky_2018",
+    override_boundary_errors = TRUE,
+    race_levels = list(white = c(1, 3, 5), black = c(2, 4)),
+    sex_levels = list(male = 0, female = 1),
+    smoke_current_levels = list(no = 0, yes = 1),
+    bp_meds_levels =  list(no = 0, yes = 1),
+    diabetes_levels = list(no = 0, yes = 1)
+  )
+  globorisk <- globorisk(
+    sex = sex,
+    # globorisk always throws an error when the age is lower than 40
+    age = ifelse(age_years >= 40, age_years, NA_integer_),
+    sbp = bp_sys_mmhg,
+    tc = chol_total_mgdl * 10 / 386.65354,
+    dm = diabetes,
+    smk = smoke_current,
+    iso = "BRA",
+    year = 2013,
+    version = "lab",
+    type = "risk",
+    updated_lac = TRUE
+  )
+})
